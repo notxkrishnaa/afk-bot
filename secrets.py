c56546cfff1ef96a11202d387f477d82f7af8485
notxkrishnaa <krishnaonly999@gmail.com>
2025-06-10 03:36:06 +0530

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -6,8 +6,8 @@ if not os.environ.get("TOKEN") or not os.environ.get("DATABASE_URL"):
 
 from telegram.ext import Filters
 
-BOT_TOKEN = os.environ.get("TOKEN")
-DB_URI = os.environ.get("DATABASE_URL")
+BOT_TOKEN = os.environ.get("7760196426:AAH3REc7UtXj_YiA0VuJpV2GwCorg9dvsCg")
+DB_URI = os.environ.get("mongodb+srv://krishnaonly999:Krishdiya07@cluster0.h4rzpxv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 SUDO_USERS = [
     1412086585
 ]
