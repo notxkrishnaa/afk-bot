ede18e411f8f67340c59f19668f8a269990714bc
notxkrishnaa <krishnaonly999@gmail.com>
2025-06-10 22:19:56 +0530

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -6,10 +6,8 @@ if not os.environ.get("TOKEN") or not os.environ.get("DATABASE_URL"):
 
 from telegram.ext import Filters
 
-BOT_TOKEN = os.environ.get("TOKEN")
-DB_URI = os.environ.get("DATABAS_URL")
-SUDO_USERS = [
-    1412086585
-]
+BOT_TOKEN = "TOKEN"
+DB_URI = "DATABAS_URL"
+SUDO_USERS = 
 SUDO = Filters.user(SUDO_USERS)
-LOG_CHAT = -1001336747262
+LOG_CHAT = -1002153866511
