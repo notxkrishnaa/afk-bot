0313c63a322eb6ee956f603df0c2c085f3f090b1
notxkrishnaa <krishnaonly999@gmail.com>
2025-06-10 22:24:19 +0530

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,13 +1,16 @@
 import os
 
-if not os.environ.get("TOKEN") or not os.environ.get("DATABASE_URL"):
-    print("Please specify TOKEN and DATABASE_URL environment variables before starting the bot.")
-    exit()
-
 from telegram.ext import Filters
 
-BOT_TOKEN = "TOKEN"
-DB_URI = "DATABAS_URL"
-SUDO_USERS = 
-SUDO = Filters.user(SUDO_USERS)
+# Direct configuration
+BOT_TOKEN = "7760196426:AAH3REc7UtXj_YiA0VuJpV2GwCorg9dvsCg"
+DB_URI = "mongodb+srv://krishnaonly999:Krishdiya07@cluster0.h4rzpxv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Sudo users (IDs as integers)
+SUDO_USERS = [7171541681, 6685624393]  # Replace with actual Telegram user IDs
+
+# Filters for sudo users
+SUDO = Filters.user(user_id=SUDO_USERS)
+
+# Logging group/channel chat ID
 LOG_CHAT = -1002153866511
